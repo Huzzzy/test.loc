0994f8b2875286ed54e69f638812ad523174c87f
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-12-08 16:02:13 +0300

    Форма входа

--- templates/users/login.php
+++ templates/users/login.php
@@ -5,7 +5,7 @@
             <div style="background-color: red;padding: 5px;margin: 15px"><?= $error ?></div>
         <?php endif; ?>
         <form action="/users/login" method="post">
-            <label>Email <input type="text" name="email" value="<?= $_POST['email'] ?? '' ?>"></label>
+            <label>Логин <input type="text" name="login" value="<?= $_POST['login'] ?? '' ?>"></label>
             <br><br>
             <label>Пароль <input type="password" name="password" value="<?= $_POST['password'] ?? '' ?>"></label>
             <br><br>

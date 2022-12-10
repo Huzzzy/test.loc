b19289a7bcaa6f10963868950dac2d33a9f08f06
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-12-10 14:26:31 +0300

    Главная страница

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -4,11 +4,12 @@ namespace MyProject\Controllers;
 
 
 
+use MyProject\Services\Db;
+
 class MainController extends AbstractController
 {
     public function main()
     {
-
         $this->view->renderHtml('main/main.php', []);
     }
 }

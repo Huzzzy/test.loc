271729921acbcf63dd7c7bb570c7a7bedcf87f16
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-12-10 22:11:15 +0300

    Создание записи в БД

--- src/MyProject/Models/ActiveRecordEntity.php
+++ src/MyProject/Models/ActiveRecordEntity.php
@@ -10,29 +10,14 @@ abstract class ActiveRecordEntity
     protected $id;
 
     /**
-     * @return int
+     * @return string
      */
-    public function getId(): int
+    public function getId(): string
     {
         return $this->id;
     }
 
-    public function __set(string $name, $value)
-    {
-        $camelCaseName = $this->underscoreToCamelCase($name);
-        $this->$camelCaseName = $value;
-    }
-
-    private function underscoreToCamelCase(string $source): string
-    {
-        return lcfirst(str_replace('_', '', ucwords($source, '_')));
-    }
-    private function camelCaseToUnderscore(string $source): string
-    {
-        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $source));
-    }
-
-    private function mapPropertiesToDbFormat(): array
+    private function mappedProperties(): array
     {
         $reflector = new \ReflectionObject($this);
         $properties = $reflector->getProperties();
@@ -40,8 +25,7 @@ abstract class ActiveRecordEntity
         $mappedProperties = [];
         foreach ($properties as $property) {
             $propertyName = $property->getName();
-            $propertyNameAsUnderscore = $this->camelCaseToUnderscore($propertyName);
-            $mappedProperties[$propertyNameAsUnderscore] = $this->$propertyName;
+            $mappedProperties[$propertyName] = $this->$propertyName;
         }
 
         return $mappedProperties;
@@ -49,126 +33,44 @@ abstract class ActiveRecordEntity
 
     public function save(): void
     {
-        $mappedProperties = $this->mapPropertiesToDbFormat();
-        if ($this->id !== null) {
-          $this->update($mappedProperties);
-        } else {
-            $this->insert($mappedProperties);
-        }
+        $properties = $this->mappedProperties();
+        
+        
+        // if ($this->id !== null) {
+        //     $this->update($properties);
+        //   } else {
+              $this->insert($properties);
+          //}
     }
 
     private function update(array $mappedProperties): void
     {
-        $columns2params = [];
-        $params2values = [];
-        $index = 1;
-        foreach ($mappedProperties as $column => $value) {
-            $param = ':param' . $index; // :param1
-            $columns2params[] = $column . ' = ' . $param; // column1 = :param1
-            $params2values[$param] = $value; // [:param1 => value1]
-            $index++;
-        }
-        $sql = 'UPDATE ' . static::getTableName() . ' SET ' . implode(', ', $columns2params) . ' WHERE id = ' . $this->id;
-        $db = Db::getInstance();
-        $db->query($sql, $params2values, static::class);
+
     }
 
-    private function insert(array $mappedProperties): void
-    {
-        $filteredProperties = array_filter($mappedProperties);
-    
-        $columns = [];
-        $paramsNames = [];
-        $params2values = [];
-        foreach ($filteredProperties as $columnName => $value) {
-            $columns[] = '`' . $columnName. '`';
-            $paramName = ':' . $columnName;
-            $paramsNames[] = $paramName;
-            $params2values[$paramName] = $value;
-        }
-    
-        $columnsViaSemicolon = implode(', ', $columns);
-        $paramsNamesViaSemicolon = implode(', ', $paramsNames);
-    
-        $sql = 'INSERT INTO ' . static::getTableName() . ' (' . $columnsViaSemicolon . ') VALUES (' . $paramsNamesViaSemicolon . ');';
-    
-        $db = Db::getInstance();
-        $db->query($sql, $params2values, static::class);
-        $this->id = $db->getLastInsertId();
-        $this->refresh();
-    }
-    private function refresh(): void
-    {
-        $objectFromDb = static::getById($this->id);
-        $reflector = new \ReflectionObject($objectFromDb);
-        $properties = $reflector->getProperties();
 
-        foreach ($properties as $property) {
-            $property->setAccessible(true);
-            $propertyName = $property->getName();
-            $this->$propertyName = $property->getValue($objectFromDb);
-        }
-    }
-
-    public function delete(): void
+    private function insert(array $properties): void
     {
         $db = Db::getInstance();
-        $db->query(
-            'DELETE FROM `' . static::getTableName() . '` WHERE id = :id',
-            [':id' => $this->id]
-        );
-        $this->id = null;
+        $db->insertToDb($properties);
     }
+
+
+    // public function refresh(): void
+    // {
+    //     $db = Db::getInstance();
+    //     $db->updateDb(array($this));
+    // }
+
 
     /**
-     * @return static[]
-     */
-    public static function findAll(): array
-    {
-        $db = Db::getInstance();
-        return $db->query('SELECT * FROM `' . static::getTableName() . '`;', [], static::class);
-    }
-
-    abstract protected static function getTableName(): string;
-
-    /**
-     * @param int $id
+     * @param string $id
      * @return static|null
      */
-    public static function getById(int $id): ?self
+    public static function getById(string $id): ?self
     {
         $db = Db::getInstance();
-        $entities = $db->query(
-            'SELECT * FROM `' . static::getTableName() . '` WHERE id=:id;',
-            [':id' => $id],
-            static::class
-        );
+        $entities  = $db->getUserId($id);
         return $entities ? $entities[0] : null;
-    }
-
-    public static function findOneByColumn(string $columnName, $value): ?self
-    {
-        $db = Db::getInstance();
-        $result = $db->query(
-            'SELECT * FROM `' . static::getTableName() . '` WHERE `' . $columnName . '` = :value LIMIT 1;',
-            [':value' => $value],
-            static::class
-        );
-        if ($result === []) {
-            return null;
-        }
-        return $result[0];
-    }
-
-    public static function findTheLastThreeColumns(): array
-    {
-        $db = Db::getInstance();
-        $result = $db->query('SELECT * FROM `' . static::getTableName() .
-            '`ORDER BY `id` DESC LIMIT 3;',
-            [],
-            static::class
-        );
-
-        return $result;
     }
 }
